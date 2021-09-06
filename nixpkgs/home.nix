dffc2afbccb94dcb550b8cf00b52910840424571
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2021-09-06 16:07:05 +0800

    Add Git Town

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -13,12 +13,23 @@
     git
     alacritty
     i3lock-color
+    git-town
   ];
 
   programs.git = {
     enable = true;
     userName = "Dylan R. Johnston";
     userEmail = "dylan.r.johnston@gmail.com";
+    aliases = {
+      append = "town append";
+      hack = "town hack";
+      kill = "town kill";
+      new-pull-request = "town new-pull-request";
+      prune-branch = "town prune-branch";
+      repo = "town repo";
+      ship = "town ship";
+      sync = "town sync";
+    };
     extraConfig = {
       init = {
         defaultBranch = "main";
