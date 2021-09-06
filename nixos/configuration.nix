46d0b3a50ece4d712b28cc17ee19783d84de3a13
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2021-09-06 10:05:08 +0800

    Initial Commit

---
+++ nixos/configuration.nix
@@ -0,0 +1,129 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+
+  users.users.dylanj.isNormalUser = true;
+  users.users.dylanj.extraGroups = [ "wheel" "networkmanager" ];
+
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;
+  # Set your time zone.
+  time.timeZone = "Australia/Perth";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.eno2.useDHCP = true;
+  networking.interfaces.enp8s0u2u4.useDHCP = true;
+  networking.interfaces.wlo1.useDHCP = true;
+
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    xkbOptions = "eurosign:e"; 
+
+    displayManager.lightdm.enable = true;
+    displayManager.defaultSession = "none+i3";
+    
+    desktopManager.xterm.enable = false;
+    
+    windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3-gaps;
+
+      extraPackages = with pkgs; [
+        dmenu
+        i3status
+        i3lock
+        i3blocks
+        i3lock-fancy
+      ];
+    };
+  };
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+  
+    #services.xserver.enable = true;
+  #services.xserver.displayManager.lightdm.enable = true;
+  #services.xserver.desktopManager.xterm.enable = true;
+
+  # Configure keymap in X11
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.jane = {
+  #   isNormalUser = true;
+  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  # };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+   environment.systemPackages = with pkgs; [
+     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+   ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05"; # Did you read the comment?
+
+}
+
---
+++ nixpkgs/home.nix
@@ -0,0 +1,93 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    htop
+    fortune
+    google-chrome
+    _1password
+    vim
+    tmux
+    vscode
+    feh
+    git
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "Dylan R. Johnston";
+    userEmail = "dylan.r.johnston@gmail.com";
+    extraConfig = {
+      init = {
+        defaultBranch = "main";
+      };
+    };
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 1800;
+    enableSshSupport = true;
+  };
+
+  programs.home-manager = {
+    enable = true;
+    path = "…";
+  };
+
+  programs.autorandr = {
+    enable = true;
+    profiles = {
+      "undocked" = {
+        fingerprint = {
+          "eDP-1" = "00ffffffffffff0009e5bc0700000000121c0104951f1178020ea0a657529f2710505400000001010101010101010101010101010101d93880b470384b403020360035ad1000001a00000000000000000000000000000000001a000000fe003333385847804e4531344e343400000000000041019e001000000a010a2020008a";
+        };
+        config = {
+          "eDP-1" = { enable = true; };
+        };
+      };
+      "docked" = {
+        fingerprint = {
+          "eDP-1" = "00ffffffffffff0009e5bc0700000000121c0104951f1178020ea0a657529f2710505400000001010101010101010101010101010101d93880b470384b403020360035ad1000001a00000000000000000000000000000000001a000000fe003333385847804e4531344e343400000000000041019e001000000a010a2020008a";
+          "DP-1-2" = "00ffffffffffff0010ac81a14c413430271e0104b55021783e5d15ae4f44ab270e5054a54b00714f81008180a940d1c0010101010101e77c70a0d0a0295030203a00204f3100001a000000ff0043485a4b3635330a2020202020000000fc0044454c4c20553334323157450a000000fd0030551e5920010a202020202020012a02031df1509005040302071601141f1213454b4c5a2309070783010000565e00a0a0a0295030203500204f3100001a584d00b8a1381440942cb500204f3100001e3c41b8a060a029505020ca04204f3100001a023a801871382d40582c4500204f3100001e0000000000000000000000000000000000000000000000000000d5";
+        };
+        config = {
+          "eDP-1" = { enable = false; };
+          "DP-1-2" = {
+            enable = true;
+            crtc = 0;
+            primary = true;
+            position = "0x0";
+            mode = "3440x1440";
+            rate = "60.00";
+          };
+        };
+      };
+    };
+  };
+
+  xsession.enable = true;
+  xsession.windowManager.i3 = {
+    enable = true;
+    package = pkgs.i3-gaps;
+
+    extraConfig = ''
+      bindsym Mod4+L exec $HOME/.config/nixpkgs/scripts/i3lock-solarized-dark.sh
+      exec_always autorandr --change
+      exec_always --no-startup-id sleep 5 && feh --bg-center --no-xinerama $HOME/Pictures/background.png
+    '';
+
+    config = rec {
+      modifier = "Mod4";
+      bars = [ ];
+      terminal = "alacritty";
+
+      window.border = 0;
+
+      gaps = {
+        inner = 15;
+        outer = 0;
+      };
+    };
+  };
+}
