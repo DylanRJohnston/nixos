9605a5da11c61df3ec5e70b5e5e68e4a9e7d6cb5
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2021-09-16 11:45:53 +0800

    Switch to pwoerline9k

--- nixpkgs/config/zsh.nix
+++ nixpkgs/config/zsh.nix
@@ -5,8 +5,8 @@
     enableSyntaxHighlighting = true;
     oh-my-zsh = {
       enable = true;
-      theme = "agnoster";
     };
+    initExtra = "source ${pkgs.zsh-powerlevel9k}/share/zsh-powerlevel9k/powerlevel9k.zsh-theme";
     plugins = [{
       name = "enhancd";
       file = "init.sh";
@@ -17,5 +17,13 @@
         sha256 = "1smskx9vkx78yhwspjq2c5r5swh9fc5xxa40ib4753f00wk4dwpp";
       };
     }];
+    sessionVariables = {
+      DEFAULT_USER = "dylanj";
+      POWERLEVEL9K_SHORTEN_DIR_LENGTH = 1;
+      POWERLEVEL9K_SHORTEN_DELIMITER = "";
+      POWERLEVEL9K_SHORTEN_STRATEGY = "truncate_from_right";
+      POWERLEVEL9K_LEFT_PROMPT_ELEMENTS = [ "context" "dir" "vcs" ];
+      POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS = [ "status" "root_indicator" "background_jobs" "history" ];
+    };
   };
 }
