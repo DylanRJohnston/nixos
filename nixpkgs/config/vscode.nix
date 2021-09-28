154feca0f679e2fc53d9051d88922e548a3fccb4
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2021-09-28 17:08:08 +0800

    Refactor vscode plugins

--- nixpkgs/config/vscode.nix
+++ nixpkgs/config/vscode.nix
@@ -1,49 +1,27 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   programs.vscode = {
     enable = true;
     package = pkgs.vscode;
 
-    extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "nix-ide";
-        publisher = "jnoortheen";
-        version = "0.1.16";
-        sha256 = "04ky1mzyjjr1mrwv3sxz4mgjcq5ylh6n01lvhb19h3fmwafkdxbp";
-      }
-      {
-        name = "rust-analyzer";
-        publisher = "matklad";
-        version = "0.2.735";
-        sha256 = "0qr6aw99r95cdkmrryd5xm8ap3bf3kmisl4iph64ivvs833cmjk9";
-      }
-      {
-        name = "nix-env-selector";
-        publisher = "arrterian";
-        version = "1.0.7";
-        sha256 = "0mralimyzhyp4x9q98x3ck64ifbjqdp8cxcami7clvdvkmf8hxhf";
-      }
-      # Themes
-      {
-        name = "github-vscode-theme";
-        publisher = "GitHub";
-        version = "4.2.1";
-        sha256 = "08n7jhm4ixfxjpfdwly6q15n4y3cga4r0qbl5qxh69s4c80370fh";
-      }
-      #Icons
-      {
-        name = "material-icon-theme";
-        publisher = "PKief";
-        version = "4.10.0";
-        sha256 = "119zpdx2hx9d1xb3d9d773b88i2awr62ivmmnhxm9zp77x9y6b70";
-      }
+    extensions = with pkgs.vscode-extensions; [
+      esbenp.prettier-vscode
+      jnoortheen.nix-ide
+      matklad.rust-analyzer
+      arrterian.nix-env-selector
+      dbaeumer.vscode-eslint
+      github.github-vscode-theme
+      pkief.material-icon-theme
     ];
 
     userSettings = {
+      update.mode = "manual";
       editor = {
         fontFamily = "FiraCode Nerd Font Mono";
         fontLigatures = true;
         tabSize = 2;
         formatOnSave = true;
+        codeActionsOnSave.source.fixAll = true;
       };
 
       window.titlebarStyle = "custom";
@@ -52,6 +30,10 @@
       workbench = {
         colorTheme = "Solarized Dark";
         iconTheme = "material-icon-theme";
+      };
+
+      "[typescript]" = {
+        editor.defaultFormatter = "esbenp.prettier-vscode";
       };
     };
   };
