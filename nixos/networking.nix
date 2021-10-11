aa8702730371da00babbe77ac10c79a40b608b33
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2021-10-11 15:22:03 +0800

    Move network config out of nixos

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -14,21 +14,9 @@
     networkmanager = {
       enable = true;
       extraConfig = ''
-        [connection-wifi-spaarc]
-        match-device=interface-name:wlo1
-        ipv4.route-metric=99
-
-        [connection-ethernet-spaarc]
-        match-device=interface-name:enp8s0u2u4
-        route1=172.23.0.0/16,172.23.18.254
+        [device-mac-randomization]
+        wifi.scan-rand-mac-address=no
       '';
     };
-
-    # interfaces.enp8s0u2u4.useDHCP = false;
-    # interfaces.enp8s0u2u4.ipv4.routes = [{
-    #   address = "172.23.0.0";
-    #   prefixLength = 16;
-    #   via = "172.23.18.254";
-    # }];
   };
 }
