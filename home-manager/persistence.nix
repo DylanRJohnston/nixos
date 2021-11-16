48396c94cec805e116c2ab923afed0f9345f5bfb
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2021-11-16 11:15:18 +0800

    Persist more home directory files

--- home-manager/persistence.nix
+++ home-manager/persistence.nix
@@ -1,13 +1,19 @@
 {
   home.persistence."/nix/persist/home/dylanj" = {
+    allowOther = false;
+
     directories = [
       "Downloads"
       "Pictures"
       ".ssh"
       "Workspace"
+      ".config/1Password"
+      ".enhancd"
+      ".mozilla"
     ];
     files = [
       ".zsh_history"
+      ".cache/rofi-3.runcache"
     ];
   };
 }
