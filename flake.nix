1771922c7c7edaa9a58bbf23fc1a17bca7a5808f
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2021-12-07 14:17:12 +0800

    Update to 21.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
