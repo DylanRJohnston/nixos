c1bdff483b8f7a9d8eec4632201bb8029f5e3534
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2022-01-06 10:41:47 +0800

    Add home autorandr config

--- dylanj/config/autorandr.nix
+++ dylanj/config/autorandr.nix
@@ -1,8 +1,10 @@
 { lib, ... }:
 let
-  internalDisplay = { "eDP-1" = "*8a"; };
-  externalMonitor = { "DP-1-2" = "*d5"; };
-  displays = lib.genAttrs [ "DP-1" "DP-2" "DP-3" "DP-1-1" "DP-1-2" "DP-1-3" "HDMI-1" "HDMI-2" "eDP-1" ] (_: { enable = false; });
+  laptop = { "eDP-1" = "*8a"; };
+  work = { "DP-1-2" = "*d5"; };
+  boardroom = { "DP-3" = "*ca"; };
+  home = { "DP-1" = "*3e"; };
+  allOff = lib.genAttrs [ "DP-1" "DP-2" "DP-3" "DP-1-1" "DP-1-2" "DP-1-3" "HDMI-1" "HDMI-2" "eDP-1" ] (_: { enable = false; });
 in
 {
   programs.autorandr = {
@@ -12,8 +14,8 @@ in
 
     profiles = {
       "undocked" = {
-        fingerprint = internalDisplay;
-        config = displays // {
+        fingerprint = laptop;
+        config = allOff // {
           "eDP-1" = {
             enable = true;
             crtc = 1;
@@ -23,9 +25,9 @@ in
           };
         };
       };
-      "docked" = {
-        fingerprint = internalDisplay // externalMonitor;
-        config = displays // {
+      "docked-work" = {
+        fingerprint = laptop // work;
+        config = allOff // {
           "DP-1-2" = {
             enable = true;
             crtc = 0;
@@ -33,6 +35,32 @@ in
             position = "0x0";
             primary = true;
             rate = "59.97";
+          };
+        };
+      };
+      "docked-home" = {
+        fingerprint = laptop // home;
+        config = allOff // {
+          "DP-1" = {
+            enable = true;
+            crtc = 1;
+            mode = "3440x2160";
+            position = "0x0";
+            primary = true;
+            rate = "60.00";
+          };
+        };
+      };
+      "docked-boardroom" = {
+        fingerprint = laptop // boardroom;
+        config = allOff // {
+          "DP-3" = {
+            enable = true;
+            crtc = 0;
+            mode = "1920x1080";
+            position = "0x0";
+            primary = true;
+            rate = "60.00";
           };
         };
       };
