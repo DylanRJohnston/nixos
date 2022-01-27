9211787a738b5f67293ccd5d75d21074ff226927
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2022-01-27 14:58:07 +0800

    Remove dnsmasq

--- hosts/dylanj-work-dell/nixos/networking.nix
+++ hosts/dylanj-work-dell/nixos/networking.nix
@@ -1,14 +1,4 @@
 {
-  services.dnsmasq = {
-    enable = true;
-    resolveLocalQueries = true;
-    servers = [
-      "/.fugro/172.23.18.10"
-      "8.8.8.8"
-      "8.8.4.4"
-    ];
-  };
-
   networking = {
     hostName = "dylanj-work-dell";
 
