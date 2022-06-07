fc3acdf36a7c1193bbbe3977020283bb37a20590
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2022-06-07 08:54:29 +0000

    Fix code-server user data dir

--- common/nixos/code-server.nix
+++ common/nixos/code-server.nix
@@ -4,7 +4,7 @@
     host = "10.55.0.1";
     user = "dylanj";
     auth = "none";
-    extraArguments = [ "--disable-telemetry" "--user-data-dir" "/home/dylanj/.vscode" ];
+    extraArguments = [ "--disable-telemetry" "--user-data-dir" "/home/dylanj/.config/Code" ];
 
   };
 
