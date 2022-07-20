22829404aa06be0d638c6fda793f6391b2f82cb2
Dylan R. Johnston <dylan.johnston@familyzone.com>
2022-07-20 14:48:48 +0800

    Remove deprecated font_antialias from wezterm

--- common/home-manager/wezterm.nix
+++ common/home-manager/wezterm.nix
@@ -6,7 +6,6 @@
       color_scheme = "Solarized Dark Higher Contrast",
       enable_tab_bar = false,
       font = wezterm.font('FiraCode Nerd Font Mono', { weight = 'Bold' }),
-      font_antialias = "Subpixel",
       font_size = 16,
       window_decorations = "RESIZE",
     }
