fcb273629bed34d00966e9ebec68cf442e750ad0
Dylan R. Johnston <dylan.johnston@familyzone.com>
2023-10-31 14:09:24 +0800

    Add dash-to-dock config

--- hosts/steamdeck/home-manager.nix
+++ hosts/steamdeck/home-manager.nix
@@ -10,7 +10,27 @@
         screen-keyboard-enabled = true;
       };
       "org/gnome/shell" = {
-        favourite-apps = [ "steam.desktop" ];
+        enabled-extensions = [
+          "dash-to-dock@micxgx.gmail.com"
+        ];
+        favourite-apps = [ "steam.desktop" "Gaming Mode.desktop" ];
+      };
+      "org/gnome/shell/extensions/dash-to-dock" = {
+        background-opacity = 0.80000000000000004;
+        custom-theme-shrink = true;
+        dash-max-icon-size = 48;
+        dock-fixed = true;
+        dock-position = "LEFT";
+        extend-height = true;
+        height-fraction = 0.60999999999999999;
+        hot-keys = false;
+        preferred-monitor = -2;
+        preferred-monitor-by-connector = "eDP-1";
+        scroll-to-focused-application = true;
+        show-apps-at-top = true;
+        show-mounts = true;
+        show-show-apps-button = true;
+        show-trash = false;
       };
     };
 }
