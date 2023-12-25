eadaa00fb43f55fe2d4bb0f0fc1067e95ae234ea
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2023-12-25 12:06:02 +0800

    Add new macbook ssh keys

--- common/nixos/openssh.nix
+++ common/nixos/openssh.nix
@@ -1,6 +1,7 @@
 let keys = [
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICX6q5M2rAKRjKckyxL31RCxUFDvAZ5gYWbfxnA3rUaz dylanj@macbook-pro.lan"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDmYQdCCtCy0xwlPdYIxF/0N7bP2XzH7WgTFAxL5Jwo/ nixos@DYLAN-DESKTOP"
+  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGGm9bXqjKjMATEaJ0mcRIxVJ0bzJ3plmd3RZvvgwtPl dylanj@Dylans-MacBook-Pro.local"
 ];
 in {
   programs.mosh.enable = true;
