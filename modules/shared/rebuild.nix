a1425a9c1352247a495a33d01dc69d990df1d461
Dylan Johnston <dylan.r.johnston@gmail.com>
2026-02-11 15:01:44 +0800

    feat(shared/rebuild): add darwin support

--- modules/shared/rebuild.nix
+++ modules/shared/rebuild.nix
@@ -8,13 +8,20 @@ let
   inherit (lib) mkEnableOption mkIf;
 
   cfg = config.custom.fonts;
+  rebuild =
+    if pkgs.stdenv.isLinux then
+      "/run/current-system/sw/bin/nixos-rebuild"
+    else if pkgs.stdenv.isDarwin then
+      "/run/current-system/sw/bin/darwin-rebuild"
+    else
+      throw "Unsupported platform, isLinux and isDarwin are both false";
 in
 {
   options.custom.rebuild.enable = mkEnableOption "Enable passwordless rebuild";
 
   config = mkIf cfg.enable {
     security.sudo.extraConfig = ''
-      ALL ALL=NOPASSWD: /run/current-system/sw/bin/nixos-rebuild
+      ALL ALL=NOPASSWD: ${rebuild}
     '';
   };
 }
