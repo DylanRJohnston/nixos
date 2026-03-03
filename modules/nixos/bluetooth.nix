b14ff738e9b40ff3f4bbc20ac6f7b9a8d20d7404
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2026-03-03 23:27:01 +0800

    feat(nixos/base): enable bluetooth stack

---
+++ modules/nixos/bluetooth.nix
@@ -0,0 +1,16 @@
+{
+  flake.modules.nixos.base.hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = true;
+      };
+      Policy = {
+        AutoEnable = true;
+      }; 
+    };
+  };
+}
