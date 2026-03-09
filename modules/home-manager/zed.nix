0faab93895fdc3431aa6d4f90f61e85b8b6ec7bd
Dylan Johnston <dylan.r.johnston@gmail.com>
2026-03-09 14:41:41 +0800

    feat(home-manager/zed): switch to modular zed configuration

--- modules/home-manager/zed.nix
+++ modules/home-manager/zed.nix
@@ -1,8 +1,66 @@
 {
   flake.modules.homeManager.base =
-    { config, ... }:
+    { config, lib, ... }:
     {
-      home.file.".config/zed/settings.json".source =
-        config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nixpkgs/mutable/zed/settings.json";
+      options.custom.zed.config = lib.mkOption {
+        type = lib.types.submodule {
+          freeformType = lib.types.anything;
+        };
+      };
+
+      config = {
+        custom.zed.config = {
+          context_servers.context7 = {
+            enabled = true;
+            url = "https://mcp.context7.com/mcp";
+          };
+
+          agent = {
+            default_profile = "ask";
+            model_parameters = [ ];
+            default_model = {
+              effort = "medium";
+              enable_thinking = true;
+              provider = "zed.dev";
+              model = "gpt-5.3-codex";
+            };
+          };
+
+          ui_font_size = 16;
+          buffer_font_size = 16;
+
+          theme = {
+            mode = "dark";
+            light = "Ayu Light";
+            dark = "Catppuccin Macchiato";
+          };
+
+          buffer_font_family = "FiraCode Nerd Font Mono";
+
+          languages = {
+            Nix.language_servers = [
+              "nixd"
+              "!nil"
+            ];
+            TypeScript.code_actions_on_format = {
+              "source.fixAll.eslint" = true;
+            };
+            Starlark.language_servers = [
+              "starpls"
+              "!tilt"
+            ];
+          };
+
+          lsp.nixd.initialization_options.formatting.command = [ "nixfmt" ];
+
+          auto_install_extensions = {
+            nix = true;
+            rust = true;
+            toml = true;
+          };
+        };
+
+        home.file.".config/zed/settings.json".text = lib.generators.toJSON { } config.custom.zed.config;
+      };
     };
 }
