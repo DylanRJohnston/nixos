786e32ca80f9e0cb9b9adfb16fb3be6ba46858ed
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2026-03-16 23:50:22 +0800

    feat(boot): add limit for boot configuration count

--- modules/nixos/bootloader.nix
+++ modules/nixos/bootloader.nix
@@ -1,6 +1,9 @@
 {
   den.aspects.base.nixos = {
-    boot.loader.systemd-boot.enable = true;
+    boot.loader.systemd-boot = {
+      enable = true;
+      configurationLimit = 10;
+    };
     boot.loader.efi.canTouchEfiVariables = true;
   };
 }
