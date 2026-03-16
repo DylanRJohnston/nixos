e6c84339076c7e5b4d5337612e4e1329a15f5fa4
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2026-03-16 14:07:14 +0800

    refactor(zsh): switch to platform specific aspects

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -7,36 +7,21 @@
         programs.zsh.enable = true;
       };
 
+    nixos.homeManager.programs.zsh.shellAliases = {
+      "battery" = "cat /sys/class/power_supply/BAT0/capacity";
+      "pbcopy" = "xclip -i -selection clipboard";
+      "pbpaste" = "xclip -o -selection clipboard";
+      "headphones" =
+        "bluetoothctl connect 70:26:05:E0:AC:84 && sleep 2 && bluetoothctl connect 70:26:05:E0:AC:84";
+    };
+
+    darwin.homeManager.programs.zsh.sessionVariables = {
+      PATH = "$PATH:/opt/homebrew/bin";
+      APPLE_SSH_ADD_BEHAVIOR = "macos";
+    };
+
     homeManager =
-      { pkgs, lib, ... }:
-      let
-        aliases = {
-          shared = {
-            gitlog = "git log --oneline --graph --all";
-            v = "vim $(fd --type f | fzf)";
-          };
-          linux = {
-            "battery" = "cat /sys/class/power_supply/BAT0/capacity";
-            "pbcopy" = "xclip -i -selection clipboard";
-            "pbpaste" = "xclip -o -selection clipboard";
-            "headphones" =
-              "bluetoothctl connect 70:26:05:E0:AC:84 && sleep 2 && bluetoothctl connect 70:26:05:E0:AC:84";
-          };
-          darwin = { };
-        };
-        plugins = [
-          {
-            name = "enhancd";
-            file = "init.sh";
-            src = pkgs.fetchFromGitHub {
-              owner = "b4b4r07";
-              repo = "enhancd";
-              rev = "230695f8da8463b18121f58d748851a67be19a00";
-              hash = "sha256-XJl0XVtfi/NTysRMWant84uh8+zShTRwd7t2cxUk+qU=";
-            };
-          }
-        ];
-      in
+      { pkgs, ... }:
       {
         programs.starship = {
           enable = true;
@@ -47,6 +32,10 @@
           enable = true;
           autosuggestion.enable = true;
           syntaxHighlighting.enable = true;
+          shellAliases = {
+            gitlog = "git log --oneline --graph --all";
+            v = "vim $(fd --type f | fzf)";
+          };
           oh-my-zsh = {
             enable = true;
             plugins = [
@@ -55,7 +44,18 @@
               "fzf"
             ];
           };
-          plugins = plugins;
+          plugins = [
+            {
+              name = "enhancd";
+              file = "init.sh";
+              src = pkgs.fetchFromGitHub {
+                owner = "b4b4r07";
+                repo = "enhancd";
+                rev = "230695f8da8463b18121f58d748851a67be19a00";
+                hash = "sha256-XJl0XVtfi/NTysRMWant84uh8+zShTRwd7t2cxUk+qU=";
+              };
+            }
+          ];
           sessionVariables = {
             EDITOR = "zed --wait";
             LC_ALL = "en_US.UTF-8";
@@ -63,14 +63,7 @@
             DEFAULT_USER = "dylanj";
             ENHANCD_DOT_ARG = "back";
             GOPATH = "$HOME/Workspace/go";
-            PATH = "$PATH:/opt/homebrew/bin";
-            APPLE_SSH_ADD_BEHAVIOR = "macos";
           };
-          shellAliases = lib.mkMerge [
-            aliases.shared
-            (lib.optionalAttrs pkgs.stdenv.isLinux aliases.linux)
-            (lib.optionalAttrs pkgs.stdenv.isDarwin aliases.darwin)
-          ];
           initContent = ''
             if command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
               tmux attach -t default || tmux new -s default
