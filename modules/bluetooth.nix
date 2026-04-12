bcd83bad9a46aa815c525287d8a3c72cab691f55
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2026-04-12 10:57:10 +0800

    fix(bluetooth): fix bluetooth settings

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -1,9 +1,12 @@
+{ arc, ... }:
 {
-  arc.base.nixos.hardware.bluetooth = {
+  arc.base.includes = [
+    arc.base._.bluetooth
+  ];
+
+  arc.base._.bluetooth.nixos.hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
-
-    disabledPlugins = [ "policy" ];
 
     settings = {
       General = {
@@ -12,8 +15,28 @@
       };
       Policy = {
         AutoEnable = true;
-        # ReconnectUUIDs = "";
       };
     };
   };
+
+  arc.base._.bluetooth._.debug.nixos =
+    { pkgs, ... }:
+    {
+      systemd.services.bluez-dbus-watch = {
+        description = "Watch BlueZ D-Bus calls during boot";
+        after = [
+          "dbus.service"
+          "bluetooth.service"
+        ];
+        wants = [ "bluetooth.service" ];
+        wantedBy = [ "multi-user.target" ];
+
+        serviceConfig = {
+          Type = "simple";
+          Restart = "always";
+          RestartSec = 1;
+          ExecStart = "${pkgs.dbus}/bin/dbus-monitor --system \"type='method_call',destination='org.bluez'\"";
+        };
+      };
+    };
 }
