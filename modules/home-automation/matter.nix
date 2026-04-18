0381230aa1a772647d6021c72fa4cb62ac2f5f0f
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2026-04-18 12:25:29 +0800

    fix(home-automation/matter-server): remove custom fix in favour of upstream

--- modules/home-automation/matter.nix
+++ modules/home-automation/matter.nix
@@ -7,17 +7,6 @@
       enable = true;
     };
 
-    systemd.services.matter-server = {
-      environment.SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-
-      serviceConfig.BindReadOnlyPaths = [
-        "/etc/resolv.conf"
-        "/etc/hosts"
-        "/etc/nsswitch.conf"
-        "/etc/ssl/certs"
-      ];
-    };
-
     services.tailscale-serve."matter".target = "127.0.0.1:5580";
   };
 }
