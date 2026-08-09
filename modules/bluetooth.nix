10b59685e63ad9491da61994a44d714cd61e6db6
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2026-08-09 21:14:31 +0800

    fix(bluetooth): remove attempt at high quality bluetooth audio

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -4,10 +4,6 @@
     arc.base._.bluetooth
   ];
 
-  arc.base._.bluetooth.includes = [
-    arc.base._.bluetooth._.high_quality_audio
-  ];
-
   arc.base._.bluetooth.nixos.hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
@@ -21,48 +17,6 @@
         AutoEnable = true;
       };
     };
-  };
-
-  arc.base._.bluetooth._.high_quality_audio.nixos = {
-    security.rtkit.enable = true;
-
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      jack.enable = true;
-
-      wireplumber.extraConfig = {
-        "10-bluetooth-policy" = {
-          "wireplumber.settings" = {
-            # Switches profile from A2DP to HFP/HSP when an app requests the mic
-            "bluetooth.autoswitch-to-headset-profile" = true;
-          };
-        };
-
-        "20-bluetooth-codecs" = {
-          "monitor.bluez.properties" = {
-            "bluez5.roles" = [
-              "a2dp_sink"
-              "a2dp_source"
-              "bap_sink"
-              "bap_source"
-              "hfp_hf"
-              "hfp_ag"
-            ];
-            "bluez5.codecs" = [
-              "sbc"
-              "sbc_xq"
-              "aac"
-            ];
-            "bluez5.hfphsp-backend" = "native";
-          };
-        };
-      };
-    };
-
-    services.blueman.enable = true;
   };
 
   arc.base._.bluetooth._.debug.nixos =
