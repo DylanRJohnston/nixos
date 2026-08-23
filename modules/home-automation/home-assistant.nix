a8111cd989149827383386e18725f7dcb578367a
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2026-08-23 16:09:15 +0800

    feat(home-assistant): add primary config into git

--- modules/home-automation/home-assistant.nix
+++ modules/home-automation/home-assistant.nix
@@ -4,100 +4,45 @@
     arc.home-automation._.home-assistant
   ];
 
-  arc.home-automation._.home-assistant.nixos =
-    { pkgs, ... }:
-    {
-      services.home-assistant = {
-        enable = true;
-        openFirewall = true;
-        extraComponents = [
-          "default_config"
-          "esphome"
-          "homekit"
-          "matter"
-          "met"
-          "otbr"
-          "thread"
-          "zha"
+  arc.home-automation._.home-assistant = {
+    nixos =
+      { ... }:
+      {
+        virtualisation.podman.enable = true;
+        virtualisation.oci-containers = {
+          backend = "podman";
+          containers.homeassistant = {
+            image = "ghcr.io/home-assistant/home-assistant:stable";
+            volumes = [ "/etc/nixos/modules/home-automation/config:/config" ];
+            environment.TZ = "Australia/Perth";
+            extraOptions = [
+              "--network=host"
+              # Required by Home Assistant's Bluetooth and DHCP watchers.
+              "--cap-add=NET_ADMIN"
+              "--cap-add=NET_RAW"
+              # "--device=/dev/ttyACM0:/dev/ttyACM0"
+              "--device=/dev/serial/by-id/usb-SMLIGHT_SMLIGHT_SLZB-07_c6bcdaf5dad5ef11977a694b49d2c684-if00-port0:/dev/serial/by-id/usb-SMLIGHT_SMLIGHT_SLZB-07_c6bcdaf5dad5ef11977a694b49d2c684-if00-port0"
+            ];
+          };
+        };
+
+        networking.firewall.allowedTCPPorts = [
+          8123
+          # Required for HomeKit component.
+          21064
         ];
 
-        extraPackages =
-          python3Packages: with python3Packages; [
-            afsapi
-            aiohomekit
-            aiohue
-            aiopyarr
-            androidtvremote2
-            b2sdk
-            getmac
-            gtts
-            ibeacon-ble
-            influxdb
-            isal
-            jellyfin-apiclient-python
-            pyatv
-            pychromecast
-            pysmlight
-            python-otbr-api
-            qbittorrent-api
-            samsungctl
-            samsungtvws
-            yalexs-ble
-            zha
-            zlib-ng
-          ];
+        # Expose Home Assistant as a Tailscale Service.
+        services.tailscale-serve."hass".target = "127.0.0.1:8123";
 
-        config = {
-          default_config = { };
-          http = {
-            use_x_forwarded_for = true;
-            trusted_proxies = [ "127.0.0.1" ];
-          };
-          automation = "!include automations.yaml";
-          script = "!include scripts.yaml";
-          scene = "!include scenes.yaml";
-          zha.zigpy_config.ota.extra_providers = [ { type = "ikea"; } ];
+        services.avahi = {
+          enable = true;
+          nssmdns4 = true;
+          nssmdns6 = true;
+          openFirewall = true;
         };
+
+        services.dbus.enable = true;
       };
-
-      # Required for homekit component
-      networking.firewall.allowedTCPPorts = [ 21064 ];
-
-      # Expose Home Assistant as a Tailscale Service
-      services.tailscale-serve."hass".target = "127.0.0.1:8123";
-
-      services.avahi = {
-        enable = true;
-        nssmdns4 = true;
-        nssmdns6 = true;
-        openFirewall = true;
-      };
-
-      services.dbus.enable = true;
-
-      # services.openthread-border-router = {
-      #   enable = true;
-      #   openFirewall = true;
-      #   backboneInterfaces = [ "end0" ];
-      #   web.enable = true;
-      #   radio = {
-      #     device = "/dev/serial/by-id/usb-SMLIGHT_SMLIGHT_SLZB-07_c6bcdaf5dad5ef11977a694b49d2c684-if00-port0";
-      #     baudRate = 460800;
-      #     flowControl = true;
-      #   };
-      # };
-
-      # systemd.services.otbr-agent = {
-      #   serviceConfig = {
-      #     ExecStartPost = [
-      #       "${pkgs.bash}/bin/bash -c 'for i in {1..10}; do [ -S /var/run/openthread-wpan0.sock ] && break; sleep 1; done'"
-      #       "+${pkgs.coreutils}/bin/chown root:otbr-web /var/run/openthread-wpan0.sock"
-      #       "+${pkgs.coreutils}/bin/chmod 660 /var/run/openthread-wpan0.sock"
-      #     ];
-      #   };
-      # };
-
-      # services.tailscale-serve."otbr".target = "127.0.0.1:8082";
-      # services.tailscale-serve."otbr-api".target = "127.0.0.1:8081";
-    };
+  };
 }
