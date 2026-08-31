e3793dea2ac3c5358afa9dabc0c1ca0cef123a55
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2026-08-31 11:15:25 +0800

    feat(hardware/raspberry-pi): add bluetooth support

--- modules/hardware/raspberry-pi.nix
+++ modules/hardware/raspberry-pi.nix
@@ -1,21 +1,46 @@
-{ inputs, ... }:
 {
-  arc.hardware._.raspberry-pi.nixos =
-    { pkgs, ... }:
-    {
-      imports = [
-        inputs.hardware.nixosModules.raspberry-pi-4
-      ];
+  inputs,
+  unitTest,
+  ...
+}:
+{
+  arc.hardware._.raspberry-pi.nixos = {
+    imports = [
+      inputs.hardware.nixosModules.raspberry-pi-4
+    ];
 
-      # Issue https://github.com/NixOS/nixpkgs/issues/126755#issuecomment-869149243
-      nixpkgs.overlays = [
-        (final: super: {
-          makeModulesClosure = x: super.makeModulesClosure (x // { allowMissing = true; });
-        })
-      ];
+    # Issue https://github.com/NixOS/nixpkgs/issues/126755#issuecomment-869149243
+    nixpkgs.overlays = [
+      (final: super: {
+        makeModulesClosure = x: super.makeModulesClosure (x // { allowMissing = true; });
+      })
+    ];
 
-      hardware.raspberry-pi."4" = {
-        fkms-3d.enable = true;
-      };
+    hardware.raspberry-pi."4" = {
+      bluetooth.enable = true;
+      fkms-3d.enable = true;
     };
+  };
+
+  flake.tests.raspberry-pi = {
+    test-bluetooth-enabled = unitTest (
+      { arc, igloo, ... }:
+      {
+        den.hosts.aarch64-linux.igloo.aspects = [ arc.hardware._.raspberry-pi ];
+
+        expr = igloo.hardware.raspberry-pi."4".bluetooth.enable;
+        expected = true;
+      }
+    );
+
+    test-bluetooth-disabled = unitTest (
+      { arc, igloo, ... }:
+      {
+        den.hosts.aarch64-linux.igloo.aspects = [ arc.base ];
+
+        expr = igloo.hardware.raspberry-pi."4".bluetooth.enable;
+        expectedError.msg = "attribute 'raspberry-pi' missing";
+      }
+    );
+  };
 }
