46666b811111762afe82c8b8017f89677e9594a9
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2026-09-04 18:15:09 +0800

    fix(hardware/raspberry-pi): cross build from x86 for better remote builder speed

--- modules/hardware/raspberry-pi.nix
+++ modules/hardware/raspberry-pi.nix
@@ -4,23 +4,38 @@
   ...
 }:
 {
-  arc.hardware._.raspberry-pi.nixos = {
-    imports = [
-      inputs.hardware.nixosModules.raspberry-pi-4
-    ];
+  arc.hardware._.raspberry-pi.nixos =
+    { pkgs, ... }:
+    {
+      imports = [
+        inputs.hardware.nixosModules.raspberry-pi-4
+      ];
 
-    # Issue https://github.com/NixOS/nixpkgs/issues/126755#issuecomment-869149243
+      # Build the aarch64 Raspberry Pi kernel natively on x86_64 builders.
+      boot.kernelPackages =
+        let
+          crossPkgs = import pkgs.path {
+            localSystem = "x86_64-linux";
+            crossSystem = pkgs.stdenv.hostPlatform.system;
+          };
+          kernel = crossPkgs.callPackage (inputs.hardware.outPath + "/raspberry-pi/common/kernel.nix") {
+            rpiVersion = 4;
+          };
+        in
+        crossPkgs.linuxPackagesFor kernel;
+
+      # Issue https://github.com/NixOS/nixpkgs/issues/126755#issuecomment-869149243
     nixpkgs.overlays = [
       (final: super: {
         makeModulesClosure = x: super.makeModulesClosure (x // { allowMissing = true; });
       })
     ];
 
-    hardware.raspberry-pi."4" = {
-      bluetooth.enable = true;
-      fkms-3d.enable = true;
+      hardware.raspberry-pi."4" = {
+        bluetooth.enable = true;
+        fkms-3d.enable = true;
+      };
     };
-  };
 
   flake.tests.raspberry-pi = {
     test-bluetooth-enabled = unitTest (
@@ -30,6 +45,22 @@
 
         expr = igloo.hardware.raspberry-pi."4".bluetooth.enable;
         expected = true;
+      }
+    );
+
+    test-kernel-cross-compiled = unitTest (
+      { arc, igloo, ... }:
+      {
+        den.hosts.aarch64-linux.igloo.aspects = [ arc.hardware._.raspberry-pi ];
+
+        expr = {
+          buildPlatform = igloo.boot.kernelPackages.kernel.stdenv.buildPlatform.system;
+          hostPlatform = igloo.boot.kernelPackages.kernel.stdenv.hostPlatform.system;
+        };
+        expected = {
+          buildPlatform = "x86_64-linux";
+          hostPlatform = "aarch64-linux";
+        };
       }
     );
 
