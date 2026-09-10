967fde8b9255d4907fde57e42d3bad5c00405d20
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2026-09-10 22:59:56 +0800

    test

--- modules/rebuild.nix
+++ modules/rebuild.nix
@@ -1,11 +1,29 @@
 {
+  arc,
+  lib,
+  nixosAspectTest,
+  ...
+}:
+{
   arc.base = {
     darwin.security.sudo.extraConfig = ''
       ALL ALL=NOPASSWD: /run/current-system/sw/bin/darwin-rebuild
     '';
 
     nixos.security.sudo.extraConfig = ''
-      ALL ALL=NOPASSWD: /run/current-system/sw/bin/nixos-rebuild
+      %wheel ALL=(root) NOPASSWD: /run/current-system/sw/bin/nixos-rebuild
+      %wheel ALL=(root) NOPASSWD: /nix/store/*-nixos-system-*/bin/switch-to-configuration
     '';
+  };
+
+  flake.tests.rebuild-sudo = nixosAspectTest {
+    aspects = [ arc.base ];
+    expr =
+      igloo:
+      lib.hasInfix
+        "%wheel ALL=(root) NOPASSWD: /nix/store/*-nixos-system-*/bin/switch-to-configuration"
+        igloo.security.sudo.extraConfig;
+    enabled = true;
+    disabled = false;
   };
 }
